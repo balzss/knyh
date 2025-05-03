7645937b072d7eb79bc42b468a9127411e9bd3f1
balazs.saros <balazs.saros@gmail.com>
2025-05-03 14:56:05 +0200

    minor improvements to recipe details page

--- src/app/recipes/[recipe_id]/recipe-view.tsx
+++ src/app/recipes/[recipe_id]/recipe-view.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { Users, Timer } from 'lucide-react'
+import { Users, Timer, X, Pen, EllipsisVertical, Share2, Presentation } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import { useSidebar } from '@/components/ui/sidebar'
 import { TopBar } from '@/components/TopBar'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Badge } from '@/components/ui/badge'
-import { AppSidebar, PageLayout } from '@/components/custom'
+import { AppSidebar, PageLayout, IconButton } from '@/components/custom'
 import { useRecipes, useTags } from '@/hooks'
 
 type RecipeViewProps = {
@@ -13,14 +14,62 @@ type RecipeViewProps = {
 }
 
 export default function RecipeView({ recipeId }: RecipeViewProps) {
+  const router = useRouter()
   const { toggleSidebar } = useSidebar()
   const { recipes } = useRecipes({ ids: [recipeId] })
   const { tags } = useTags()
   const recipe = recipes?.[0]
 
+  const handleClosePage = () => {
+    router.replace('/')
+  }
+
   return (
     <div className="flex w-full">
-      <TopBar onSidebarToggle={toggleSidebar} />
+      <TopBar
+        onSidebarToggle={toggleSidebar}
+        hideSidebarToggleMobile
+        customTopbarContent={
+          <div className="flex items-center gap-2">
+            <IconButton
+              iconSize="normal"
+              variant="ghost"
+              icon={<X />}
+              tooltip="Clear selection"
+              onClick={handleClosePage}
+              className="mr-auto sm:mr-0"
+            />
+            <IconButton
+              iconSize="normal"
+              variant="ghost"
+              icon={<Presentation />}
+              tooltip="More options"
+              onClick={() => {}}
+            />
+            <IconButton
+              iconSize="normal"
+              variant="ghost"
+              icon={<Pen />}
+              tooltip="More options"
+              onClick={() => {}}
+            />
+            <IconButton
+              iconSize="normal"
+              variant="ghost"
+              icon={<Share2 />}
+              tooltip="More options"
+              onClick={() => {}}
+            />
+            <IconButton
+              iconSize="normal"
+              variant="ghost"
+              icon={<EllipsisVertical />}
+              tooltip="More options"
+              onClick={() => {}}
+            />
+          </div>
+        }
+      />
       <AppSidebar />
       <main className="w-full mt-16 mx-auto">
         <PageLayout>
@@ -43,23 +92,27 @@ export default function RecipeView({ recipeId }: RecipeViewProps) {
                 ))}
             </div>
           </div>
-          <h2 className="text-xl text-muted-foreground mt-4">Ingredients</h2>
-          <ul>
-            {recipe?.ingredients.map((ingredient, index) => (
-              <li key={index} className="flex items-center space-x-2 text-md leading-8">
-                <Checkbox id={`ingredient-${index}`} />
-                <label htmlFor={`ingredient-${index}`}>{ingredient.toString()}</label>
-              </li>
-            ))}
-          </ul>
-          <h2 className="text-xl text-muted-foreground mt-4">Instructions</h2>
-          <ol className="list-decimal flex flex-col gap-3 ml-4">
-            {recipe?.instructions.map((instruction, index) => (
-              <li key={index} className="flex items-center text-md leading-6 list-item">
-                {instruction}
-              </li>
-            ))}
-          </ol>
+          <div className="flex flex-col gap-1">
+            <h2 className="text-xl text-muted-foreground mt-4">Ingredients</h2>
+            <ul>
+              {recipe?.ingredients.map((ingredient, index) => (
+                <li key={index} className="flex items-center space-x-2 text-md leading-8">
+                  <Checkbox id={`ingredient-${index}`} />
+                  <label htmlFor={`ingredient-${index}`}>{ingredient.toString()}</label>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h2 className="text-xl text-muted-foreground mt-4">Instructions</h2>
+            <ol className="list-decimal flex flex-col gap-3 ml-4">
+              {recipe?.instructions.map((instruction, index) => (
+                <li key={index} className="flex items-center text-md leading-6 list-item">
+                  {instruction}
+                </li>
+              ))}
+            </ol>
+          </div>
         </PageLayout>
       </main>
     </div>
