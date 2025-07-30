708e4e8ac239f52e5e419851d1bcfa65c3bc28cc
balazs.saros <balazs.saros@gmail.com>
2025-07-30 18:17:50 +0200

    make it possible to switch between form and raw text view when adding recipe

--- src/app/recipes/new/page.tsx
+++ src/app/recipes/new/page.tsx
@@ -1,21 +1,33 @@
 'use client'
 
+import { useSearchParams } from 'next/navigation'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { X, Ellipsis } from 'lucide-react'
 import { useSidebar } from '@/components/ui/sidebar'
 import { TopBar } from '@/components/TopBar'
 import { AppSidebar, IconButton } from '@/components/custom'
+import {
+  DropdownMenu,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import FormView from './form-view'
 import RawView from './raw-view'
 
 export default function NewPage() {
-  const inRawView = true
+  const searchParams = useSearchParams()
+  const mode = searchParams.get('mode') === 'raw' ? 'raw' : 'form'
+  const viewToRender = mode === 'raw' ? <RawView /> : <FormView />
 
   const { toggleSidebar } = useSidebar()
   const router = useRouter()
 
   const handleClosePage = () => {
-    if (window.history.length && document.referrer === '') {
+    if (window.history.length > 1 && document.referrer === '') {
       router.back()
     } else {
       router.replace('/')
@@ -37,18 +49,31 @@ export default function NewPage() {
               onClick={handleClosePage}
             />
             <span className="mr-auto sm:mr-4 font-bold">New recipe</span>
-            <IconButton
-              iconSize="normal"
-              variant="ghost"
-              icon={<Ellipsis />}
-              tooltip="More options"
-              onClick={() => {}}
-            />
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <IconButton
+                  iconSize="normal"
+                  variant="ghost"
+                  icon={<Ellipsis />}
+                  tooltip="More options"
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>More options</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                  <Link href={mode === 'raw' ? '?mode=form' : '?mode=raw'}>
+                    Switch to {mode === 'raw' ? 'form' : 'markdown'} mode
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         }
       />
       <AppSidebar path="/new" />
-      <main className="w-full mt-16 mx-auto">{inRawView ? <RawView /> : <FormView />}</main>
+
+      <main className="w-full mt-16 mx-auto">{viewToRender}</main>
     </div>
   )
 }
--- src/app/recipes/new/raw-view.tsx
+++ src/app/recipes/new/raw-view.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react'
 import Editor from 'react-simple-code-editor'
 import { highlight, languages } from 'prismjs'
@@ -5,18 +7,27 @@ import 'prismjs/components/prism-markdown'
 import 'prismjs/themes/prism-dark.css'
 import { PageLayout } from '@/components/custom'
 
+const placeHolder = `# Recipe title
+
+- first ingredient
+- second ingredient`
+
 export default function RawView() {
-  const [code, setCode] = useState(`# add you recipe\n\n- first ingredient\n- second ingredient`)
+  const [code, setCode] = useState(placeHolder)
   return (
     <PageLayout>
       <Editor
+        name="markdown-recipe-editor"
+        textareaClassName="code-editor"
+        autoFocus
         value={code}
         onValueChange={(code) => setCode(code)}
         highlight={(code) => highlight(code, languages.markdown, 'markdown')}
-        padding={12}
+        className="rounded-lg text-sm"
+        spellCheck
         style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
+          fontFamily: '"Noto Sans Mono", monospace',
+          borderRadius: 'inherit',
         }}
       />
     </PageLayout>
