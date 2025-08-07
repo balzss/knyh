a115851fd6c4c1dffa8b8dd14f73088b7c94fb05
balazs.saros <balazs.saros@gmail.com>
2025-08-07 16:46:19 +0200

    read preferred locale from user config

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,14 @@
+import fs from 'fs'
+import path from 'path'
 import { getRequestConfig } from 'next-intl/server'
+import { serverDataPath } from '@/lib/utils'
+import type { DatabaseSchema } from '@/lib/types'
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = 'en'
+  const filePath = path.join(process.cwd(), serverDataPath)
+  const fileContent = fs.readFileSync(filePath, 'utf8')
+  const allData: DatabaseSchema = JSON.parse(fileContent)
+  const locale = allData.userConfig.language || 'en'
 
   return {
     locale,
